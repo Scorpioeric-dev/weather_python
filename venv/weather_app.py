6a63548b1d73f30e637f78d48bb2ec670e727fe2
Scorpioeric-dev <52173276+Scorpioeric-dev@users.noreply.github.com>
2021-06-03 21:04:31 -0700

    Init Commit

---
+++ venv/weather_app.py
@@ -0,0 +1,35 @@
+from tkinter import *
+
+def search():
+    pass
+
+
+app= Tk()
+app.title("Weather app")
+app.geometry('700x350')
+
+city_text = StringVar()
+city_entry = Entry(app,textvariable=city_text)
+city_entry.pack()
+
+
+search_btn = Button(app, text="", width=12,command=search)
+search_btn.pack()
+
+
+location_lbl = Label(app,text='',font=('bold',20))
+location_lbl.pack()
+
+
+image = Label(app,bitmap='')
+image.pack()
+
+
+temp_lbl = Label(app, text='')
+temp_lbl.pack()
+
+weather_lbl = Label(app, text='')
+weather_lbl.pack()
+
+
+app.mainloop()
